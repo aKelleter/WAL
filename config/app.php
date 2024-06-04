178af382a00f76ff1dfbcb741da67cc38121f093
Alain Kelleter <alain.kelleter@kelleter.be>
2024-06-04 11:06:54 +0200

    Update app.php

--- config/app.php
+++ config/app.php
@@ -1,9 +1,9 @@
 <?php
-
+// https://laconsole.dev/formations/framework-php
 const APP_NAME = 'Wal&sup2; Square';
 const APP_TITLE = 'Web Application Launcher';
-const APP_VERSION = '0.0.3 (dev)';
-const APP_UPDATED = '31-05-2024 19:25';
+const APP_VERSION = '0.0.4 (dev)';
+const APP_UPDATED = '03-06-2024 08:35';
 const APP_COPYRIGHT = '2024';
 const APP_AUTHOR = "Alain KELLETER";    
 const APP_DESCRIPTION = 'Web Application Launcher est un framework PHP qui permet de créer des applications web rapidement et simplement.';
